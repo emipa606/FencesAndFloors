df3834a01ae5f7536fa2c467a77482c10b6b7b55
emipa606 <emil.offermo@litet.net>
2021-03-18 19:52:45 +0100

    Fixed the speedup for the sensor panel-tiles

---
+++ Source/FencesAndFloors/FencesAndFloors.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using Verse;
+using Verse.AI;
+
+namespace FencesAndFloors
+{
+    [HarmonyPatch(typeof(PathGrid), "CalculatedCostAt")]
+    public static class SnowUtilityPatch
+    {
+        // Token: 0x06000002 RID: 2 RVA: 0x00002078 File Offset: 0x00000278
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instrs)
+        {
+            instrs = instrs.MethodReplacer(typeof(SnowUtility).GetMethod("MovementTicksAddOn"),
+                typeof(SnowUtilityPatch).GetMethod("MovementTicksAddOnIgnoreZero"));
+            return instrs;
+        }
+
+        public static int MovementTicksAddOnIgnoreZero(SnowCategory category)
+        {
+            switch (category)
+            {
+                case SnowCategory.None:
+                    return -100;
+                case SnowCategory.Dusting:
+                    return 0;
+                case SnowCategory.Thin:
+                    return 4;
+                case SnowCategory.Medium:
+                    return 8;
+                case SnowCategory.Thick:
+                    return 12;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Source/FencesAndFloors/FencesAndFloors_Initialization.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace FencesAndFloors
+{
+    [StaticConstructorOnStartup]
+    internal class FencesAndFloors_Initialization
+    {
+        static FencesAndFloors_Initialization()
+        {
+            var harmony = new Harmony("mlie.FencesAndFloors");
+            var assembly = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(assembly);
+        }
+    }
+}
